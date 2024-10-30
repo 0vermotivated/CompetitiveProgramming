0e01af9306f8058f1414b83d82a03f87d1e3657a
0vermotivated <iakatkov123Agmail.com>
2024-10-30 21:48:19 +0300

    Just a commit

---
+++ CSES_Problem_Set_Python/DynamicProgramming/Minimizing_Coins.py
@@ -0,0 +1,17 @@
+import random
+nm = lambda: int(input())
+arri = lambda: list(map(int, input().split()))
+arrs = lambda: input().split()
+nmm = lambda: map(int, input().split())
+MOD = 10 ** 9 + 7
+INF = (2 ** 64) + 1
+
+def solve():
+    n, t = nmm()
+    lst = arri()
+    lst = sorted(lst, reverse=True)
+    r = [1]
+
+
+for _ in range(nm()):
+    solve()
